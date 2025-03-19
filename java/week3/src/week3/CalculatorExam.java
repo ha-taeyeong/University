6987a0543e8c1c50380abef435d65806eb0f82b4
ha-taeyeong <electro0218@gmail.com>
2025-03-19 11:56:48 +0900

    commit 0318 review

---
+++ java/week3/src/week3/CalculatorExam.java
@@ -0,0 +1,12 @@
+package week3;
+
+public class CalculatorExam {
+
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+		System.out.println(cal.hap(3,4,5,6,7,1));
+		System.out.println(cal.hap(5,3,8,5,10,11,5));
+		System.out.println(cal.hap(3,4,5,6,7,1));
+	}
+
+}
