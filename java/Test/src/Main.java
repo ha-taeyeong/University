fd1709d01c725d36acda4619d754b0b89d30f84e
ha-taeyeong <electro0218@gmail.com>
2025-03-24 20:31:27 +0900

    0324 commit

---
+++ java/Test/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double sc1 = sc.nextInt();
+		double sc2 = sc.nextInt();
+		double result = sc1 / sc2;
+		System.out.print(result);
+	}
+}
---
+++ python/format.py
@@ -0,0 +1,16 @@
+number = 10
+day = "three"
+# print("I ate {number} apples. so I was sick for {day} days.".format(number=10, day=3))
+
+# print("I ate {0} apples. so I was sick for {day} days.".format(10, day=3))
+
+# print("{0:<10}".format("hi"))
+
+
+y = 3.42134234
+print("{0:0.4f}".format(y))
+print("{0:10.4f}".format(y))
+print("{{ and }}".format())
+
+d = {'name':'홍길동', 'age':30}
+print(f'나의 이름은 {d["name"]}입니다. 나이는 {d["age"]}입니다.')
