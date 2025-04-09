44be100342eaa12c3c73708c1d95486753a57e60
ha-taeyeong <electro0218@email.com>
2025-04-09 11:09:01 +0900

    0408

--- javaIntelliJ/club/src/Main.java
+++ javaIntelliJ/club/src/Main.java
@@ -1,12 +1,24 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for(int i = 0; i < 50; i++) {
-            if(i % 2 != 0) {
-                System.out.println(i);
-                continue;
+        Scanner sc = new Scanner(System.in);
+
+        float[] arr = new float[1000];
+        int count = 0;
+
+        while (true) {
+            float temp = sc.nextFloat();
+            if (temp == 999) {
+                break;
             }
+            arr[count++] = temp;
         }
+
+        for (int i = 1; i < count; i++) {
+            System.out.printf("%.2f\n", arr[i] - arr[i - 1]);
+        }
+
+        sc.close();
     }
 }
