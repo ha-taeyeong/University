12f003d2ae430cbe76c39ef4ff12a2e8aefc149d
ha-taeyeong <electro0218@email.com>
2025-05-23 17:26:46 +0900

    0523

--- javaIntelliJ/week12/src/practice8/Main.java
+++ javaIntelliJ/week12/src/practice8/Main.java
@@ -3,15 +3,11 @@ package practice8;
 public class Main {
     public static void main(String[] args) {
         String str = "hallymbigdata";
-        int firstIdx = str.indexOf('a');
-        int secondIdx = str.indexOf('a', firstIdx+1);
-        System.out.println(firstIdx);
-        System.out.println(secondIdx);
-        if(firstIdx != -1 && secondIdx != -1 && secondIdx > firstIdx) {
-            String substr = str.substring(firstIdx + 1, secondIdx);
-            System.out.println("첫 번째 'a'와 두번째 'a' 사이의 문자열: " + substr);
-        } else {
-            System.out.println("문자 'a'가 두 번 이상 등장하지 않습니다.");
-        }
+        int firstIdx = str.indexOf("a");
+        int secondIdx = str.indexOf("a", firstIdx+1);
+        String substr = str.substring(firstIdx+1, secondIdx);
+        System.out.println("str = " + str);
+        System.out.println("문자 a에서 a까지의 문자는 모두 " + substr.length() + "개 입니다.");
+        System.out.println("substr = " + substr);
     }
 }
